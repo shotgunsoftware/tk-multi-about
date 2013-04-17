253dacdee07cca16bc6ef487b5ab4c12bad56bb0
manne ohrstrom <manne@shotgunsoftware.com>
2013-04-18 00:17:07 +0100

    better menu name

--- app.py
+++ app.py
@@ -20,5 +20,7 @@ class AboutTank(Application):
         """
         tk_multi_about = self.import_module("tk_multi_about")
         cb = lambda : tk_multi_about.show_dialog(self)
-        self.engine.register_command("Work Area Info...", cb, {"type": "context_menu"})
+        self.engine.register_command("Work Area Info...", cb, {"type": "context_menu",
+                                                               "description": self.description,
+                                                               "short_name": "work_area_info"})
 
