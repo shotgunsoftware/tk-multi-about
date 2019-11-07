0124e1e0502cdb8a6afddada5b797889a4fe007c
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2019-11-07 12:37:47 -0500

    blackened

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2013 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 """
@@ -19,15 +19,19 @@ import platform
 
 from tank.platform import Application
 
+
 class AboutTank(Application):
-    
     def init_app(self):
         """
         Called as the application is being initialized
         """
         tk_multi_about = self.import_module("tk_multi_about")
-        cb = lambda : tk_multi_about.show_dialog(self)
-        self.engine.register_command("Work Area Info...", cb, {"type": "context_menu", "short_name": "work_area_info"})
+        cb = lambda: tk_multi_about.show_dialog(self)
+        self.engine.register_command(
+            "Work Area Info...",
+            cb,
+            {"type": "context_menu", "short_name": "work_area_info"},
+        )
 
     @property
     def context_change_allowed(self):
@@ -35,4 +39,3 @@ class AboutTank(Application):
         Specifies that context changes are allowed.
         """
         return True
-
--- python/tk_multi_about/environment_browser.py
+++ python/tk_multi_about/environment_browser.py
@@ -1,48 +1,49 @@
 # Copyright (c) 2013 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 import tank
 import os
 import sys
 import datetime
-import threading 
+import threading
 
 
 from tank.platform.qt import QtCore, QtGui
+
 browser_widget = tank.platform.import_framework("tk-framework-widget", "browser_widget")
 
-class EnvironmentBrowserWidget(browser_widget.BrowserWidget):
 
-    
+class EnvironmentBrowserWidget(browser_widget.BrowserWidget):
     def __init__(self, parent=None):
         browser_widget.BrowserWidget.__init__(self, parent)
-    
+
     def get_data(self, data):
-    
+
         engine = self._app.engine
-            
+
         data = {}
-        
-        data["engine"] = {"name": engine.display_name,
-                          "version": engine.version,
-                          "documentation_url": engine.documentation_url,
-                          "description": engine.description
-                          }
-        
-        data["environment"] = {"name": engine.environment.get("name"),
-                               "disk_location": engine.environment.get("disk_location"),
-                               "description": engine.environment.get("description")
-                               }
-        
+
+        data["engine"] = {
+            "name": engine.display_name,
+            "version": engine.version,
+            "documentation_url": engine.documentation_url,
+            "description": engine.description,
+        }
+
+        data["environment"] = {
+            "name": engine.environment.get("name"),
+            "disk_location": engine.environment.get("disk_location"),
+            "description": engine.environment.get("description"),
+        }
+
         return data
-        
 
     def process_result(self, result):
 
@@ -52,21 +53,19 @@ class EnvironmentBrowserWidget(browser_widget.BrowserWidget):
         details = []
         details.append("<b>Engine: %s</b>" % d.get("name"))
         details.append("Version: %s" % d.get("version"))
-        details.append("Description: %s" % d.get("description"))    
+        details.append("Description: %s" % d.get("description"))
         i.set_details("<br>".join(details))
         i.data = d
         i.setToolTip("Double click for documentation.")
         i.set_thumbnail(":/res/tank_app_logo.png")
 
-        
         d = result["environment"]
 
         i = self.add_item(browser_widget.ListItem)
         details = []
         details.append("<b>Environment: %s</b>" % d.get("name"))
         details.append("Path: %s" % d.get("disk_location"))
-        details.append("Description: %s" % d.get("description"))    
+        details.append("Description: %s" % d.get("description"))
         i.set_details("<br>".join(details))
         i.data = d
         i.set_thumbnail(":/res/tank_env_logo.png")
-            
