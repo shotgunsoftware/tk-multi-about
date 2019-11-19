3162da3d1bd041d840d04e9b3754e87f91fe9fe5
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2019-11-19 15:20:59 -0500

    works...

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,4 +1,3 @@
-
 import pytest
 import subprocess
 import time
@@ -23,27 +22,20 @@ def host_application():
 
 
 @pytest.fixture
-def top_window(host_application):
+def about_box(host_application):
     before = time.time()
-    top_window_executable = "tk-run-app" if sgtk.util.is_windows() else "python"
-    while before + 15 > time.time():
-        if topwindows["python"].exists() == True:
-            return topwindows["python"]
-    raise RuntimeError("Timeout waiting for tk-run-app to launch.")
+    while before + 30 > time.time():
+        if sgtk.util.is_windows():
+            about_box = topwindows["Shotgun: Your Current Work Area"]
+        else:
+            about_box = topwindows["python"]["Shotgun: Your Current Work Area"]
 
-
-@pytest.fixture
-def about_box(top_window):
-    before = time.time()
-    while before + 15 > time.time():
-        about_box = top_window["Shotgun: Your Current Work Area"]
         if about_box.exists() == True:
-            return AboutBoxAppWrapper(about_box)
+            return AboutBoxAppWrapper(about_box.get())
     raise RuntimeError("Timeout waiting for about box to launch.")
 
 
 class AboutBoxAppWrapper(object):
-
     def __init__(self, about_box):
         self._about_box = about_box
         assert self._about_box.exists()
@@ -59,37 +51,41 @@ class AboutBoxAppWrapper(object):
 
     @property
     def context_view(self):
-        context_view = self._get_context_view("Your Current Work Context")
-        return [c.value for i, c in enumerate(context_view.children) if i % 2 == 1]
+        return self._get_context_view_content("Your Current Work Context")
 
     @property
     def active_apps_view(self):
-        context_view = self._get_context_view("Currently Running Apps")
-        return [c.value for i, c in enumerate(context_view.children) if i % 2 == 1]
+        return self._get_context_view_content("Currently Running Apps")
 
     @property
     def current_environment_view(self):
-        context_view = self._get_context_view("The Current Environment")
-        return [c.value for i, c in enumerate(context_view.children) if i % 2 == 1]
+        return self._get_context_view_content("The Current Environment")
 
     def _get_radio_button(self, name):
-        button = self._about_box.radioButtons[name]
+        if sgtk.util.is_windows():
+            button = self._about_box.tabs[name]
+        else:
+            button = self._about_box.radioButtons[name]
         assert button.exists()
         return button
 
-    def _get_context_view(self, name):
-        context_view = self._about_box[name].parent.children[1]
-        assert context_view.exists()
-        return context_view
+    def _get_context_view_content(self, name):
+        if sgtk.util.is_windows():
+            browser = self._about_box["context_browser"].get()
+            return [c[0][1].name for c in browser.children[1][0][0][0][0].children]
+        else:
+            context_view = self._about_box[name].parent.children[1]
+            assert context_view.exists()
+            return [c.value for i, c in enumerate(context_view.children) if i % 2 == 1]
 
 
 def test_context_tab(about_box):
     about_box.select_context_tab()
     assert about_box.context_view == [
-        'Project Big Buck Bunny (jf-dev.shotgunstudio.com)\nBig Buck Bunny is a short computer animated film by the Blender Institute, part of the Blender Foundation.',
-        'Asset Acorn\nAs to size, Alice hastily but Im not looking for eggs, as it spoke. As wet as ever, said Alice to herself, and fanned herself one',
-        'Pipeline Step Art\nNo Description',
-        'Task Art\nStatus: fin\nAssigned to: Artist 3'
+        "Project Big Buck Bunny (jf-dev.shotgunstudio.com)\nBig Buck Bunny is a short computer animated film by the Blender Institute, part of the Blender Foundation.",
+        "Asset Acorn\nAs to size, Alice hastily but Im not looking for eggs, as it spoke. As wet as ever, said Alice to herself, and fanned herself one",
+        "Pipeline Step Art\nNo Description",
+        "Task Art\nStatus: fin\nAssigned to: Artist 3",
     ]
 
 
@@ -101,6 +97,6 @@ def test_active_apps_tab(about_box):
 def test_environment_tab(about_box):
     about_box.select_environment_tab()
     assert about_box.current_environment_view == [
-        'Engine: tk_testengine\nVersion: Undefined\nDescription: No description available.',
-        'Environment: test\nPath: /Users/boismej/gitlocal/tk-toolchain/tk_toolchain/cmd_line_tools/tk_run_app/config/env/test.yml\nDescription: No description found.'
+        "Engine: tk_testengine\nVersion: Undefined\nDescription: No description available.",
+        "Environment: test\nPath: /Users/boismej/gitlocal/tk-toolchain/tk_toolchain/cmd_line_tools/tk_run_app/config/env/test.yml\nDescription: No description found.",
     ]
